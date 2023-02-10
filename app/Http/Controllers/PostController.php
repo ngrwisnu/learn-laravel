13d74ee323cb72533ca6ccf94fea59102cb8ec28
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-02-10 11:40:48 +0700

    learn N + 1 Problem with clockwork

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
     public function index() {
         return view('blog', [
             "title" => "Blog",
-            "posts"=> Post::all()
+            "posts"=> Post::with(['category', 'user'])->get()
         ]);
     }
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
         return view('authors_posts', [
             'title' => "User's Posts",
             'heading' => $user,
-            'posts' => $user->posts,
+            'posts' => $user->posts->load('category'),
         ]);
     }
 }
